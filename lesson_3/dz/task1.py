fc9b8246b7e303aae9c230ee544948f60e05de18
YusupovAF <YusupovAF@outlook.com>
2022-04-17 10:34:15 +0500

    Lesson3 task done

--- lesson_3/dz/task1.py
+++ lesson_3/dz/task1.py
@@ -13,10 +13,14 @@
 
 def num_translate(number: str):
     my_dict = {'one': 'один', 'two': 'два', 'three': 'три', 'four': 'четыре', 'five': 'пять', 'six': 'шесть', 'seven': 'семь', 'eight': 'восемь', 'nine': 'девять', 'ten': 'десять'}
-    return f'"{my_dict[number]}"'
+    try:
+        my_string = f'"{my_dict[number]}"'
+        return my_string
+    except KeyError:
+        return None
 
 if __name__ == "__main__":
-    print(num_translate('one'))
+    print(num_translate('onek'))
     print(num_translate('six'))
     
 
